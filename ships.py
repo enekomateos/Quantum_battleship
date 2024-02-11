413714dcb454fa383aeeedef5027eb44894638db
molendziak <104680715+molendziak@users.noreply.github.com>
2024-02-11 16:17:55 +0100

    Added descriptions

--- ships.py
+++ ships.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 class Ships:
+    '''This class is used to create the ships for the game. The ships are created with the following parameters:
+    
+    length: the length of the ship
+    x_init: the x coordinate of the initial position of the ship
+    y_init: the y coordinate of the initial position of the ship
+    orientation: the orientation of the ship, it can be either 'vertical' or 'horizontal'
+    superposition: allows to create a ship on top of another ship, it can be either True or False
+    super_pos_orientation: the orientation of the ship that is going to be superimposed, it can be either 'vertical' or 'horizontal'
+    super_posx: the x coordinate of the initial position of the ship that is going to be superimposed
+    super_posy: the y coordinate of the initial position of the ship that is going to be superimposed
+    
+    Returns: a list with the positions of the ship
+    '''
     def __init__(self, length, x_init, y_init, orientation, superposition = False, super_pos_orientation = None, super_posx = 0, super_posy = 0):
         self.length = length
         self.x_init = x_init
@@ -34,15 +47,10 @@ class Ships:
                 for i in range(0, self.length):
                     horizontal = [self.super_posy, i + self.super_posx]
                     super_pos.append(horizontal)
-            #for i in range(0, self.length):
-             #   vertical = [i + self.super_posy, self.super_posx]
-             #   super_pos.append(vertical)
+
             return [positions, super_pos]
         
         return positions
             
             
             
-        
-obj = Ships(3, 2, 2, 'vertical', True,'horizontal', 2, 1)
-print(obj.Ship())
