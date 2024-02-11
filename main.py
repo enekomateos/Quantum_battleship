67200e50c55395e5a575ae5da63f373dbfa44480
Eneko <emateos007@ikasle.ehu.eus>
2024-02-11 20:39:53 +0100

    Move most functions from main to utilities_main for better reading

--- main.py
+++ main.py
@@ -5,191 +5,10 @@
 
 import numpy as np
 from board import Board
-from ships import Ships
-import descriptions as desc
 import os
+from utilities_main import introduction, create_ships, pass_computer, situation, create_ships_computer
 
 # Functions
-
-def introduction():
-
-    """
-    Print the introduction of the game.
-    """
-
-    # Welcome message
-    desc.print_game_description()
-
-    tutorial = input("Do you need a tutorial? (y/n)")
-    if tutorial == "y":
-        desc.tutorial()
-
-    os.system('cls' if os.name == 'nt' else "printf '\033c'")    
-
-
-def create_ships(possible_ships, amount_of_ships, board):
-    
-    """
-    Create the ships for the player.
-    
-    Args:
-        possible_ships: dictionary with the possible ships and their lengths
-        amount_of_ships: dictionary with the possible ships and the amount of ships of that type that can be created
-
-    Returns:
-        list of the positions of the ships
-    """
-    all_ships = []
-    for ship_name in possible_ships:
-        os.system('cls' if os.name == 'nt' else "printf '\033c'")
-        board.print_board(True)
-        print("")
-        print("Create the ships.")
-        print("The ship is", ship_name, "and it has", possible_ships[ship_name], "positions.")
-        print("You can create", amount_of_ships[ship_name], "ships of this type.")
-        i=1
-        while i<=amount_of_ships[ship_name]:
-            print("Create the ship number", i)
-            while True:
-                try:
-                    x = int(input("Enter the vertical coordinate of the initial position of the ship: "))
-                    y = int(input("Enter the horizontal coordinate of the initial position of the ship: "))
-                    break
-                except:
-                    print("Invalid input. Please enter a number.")
-            while True:
-                try:
-                    orientation = input("Enter the orientation of the ship. It can be either 'vertical' or 'horizontal': ")
-                    break
-                except:
-                    print("Invalid input. Please enter either 'vertical' or 'horizontal' for orientation.")
-            superposition = input("Do you want to create a superposition of the ship? (y/n)")
-            if superposition == "y" or superposition == "Y" or superposition == "yes" or superposition == "Yes" or superposition == "YES":
-                print("SUPERPOSITION")
-                while True:
-                    invalid = False
-                    try:
-                        super_posx = int(input("Enter the vertical coordinate of the initial position of the ship that is going to be superimposed: "))
-                        super_posy = int(input("Enter the horizontal coordinate of the initial position of the ship that is going to be superimposed: "))
-                        super_pos_orientation = input("Enter the orientation of the ship that is going to be superimposed. It can be either 'vertical' or 'horizontal': ")
-                    except:
-                        print("Invalid input. Please enter a number and correct orientation.")
-                    pos_sup = []
-                    if super_pos_orientation == "vertical":
-                        for j in range(possible_ships[ship_name]):
-                            pos_sup.append((super_posy, super_posx+j))
-                    else:
-                        for j in range(possible_ships[ship_name]):
-                            pos_sup.append((super_posy+j, super_posx))
-                    print(pos_sup)
-                    pos = []
-                    if orientation == "vertical":
-                        for j in range(possible_ships[ship_name]):
-                            pos.append((y, x+j))
-                    else:
-                        for j in range(possible_ships[ship_name]):
-                            pos.append((y+j, x))
-                    print(pos)
-                    for j in pos:
-                        if j in pos_sup:
-                            print("Invalid position for the superposition. Please choose another position.")
-                            invalid = True
-                    if invalid:
-                        continue
-                    break
-
-                ship = Ships(possible_ships[ship_name], y, x, orientation, True, super_pos_orientation, super_posy, super_posx)
-                all_ships.append(ship)
-                if board.check_valid_position(ship):
-                    board.add_ship(ship)
-                else:
-                    print("Invalid position for the ship. Please choose another position.")
-                    continue
-                
-                input("Press enter to continue")
-            else:
-                ship = Ships(possible_ships[ship_name], y, x, orientation)
-                all_ships.append(ship)
-                if board.check_valid_position(ship):
-                    board.add_ship(ship)
-                else:
-                    print("Invalid position for the ship. Please choose another position.")
-                    continue
-            i +=1
-            input("Press enter to continue")
-    print("All the ships are created. The board looks like this:")
-    board.print_board(True)
-    return all_ships
-
-def create_ships_computer(possible_ships, amount_of_ships, board):
-
-    """
-    Creates and places the ships for the computer.
-
-    Args:
-        possible_ships: dictionary with the possible ships and their lengths
-        amount_of_ships: dictionary with the possible ships and the amount of ships of that type that can be created
-
-    Returns:
-        list of the positions of the ships
-    """
-
-    all_ships = []
-    for ship_name in possible_ships:
-        i=1
-        while i<=amount_of_ships[ship_name]:
-            x = np.random.randint(0, 10)
-            y = np.random.randint(0, 10)
-            orientation = np.random.choice(["vertical", "horizontal"])
-            superposition = np.random.choice(["y", "n"])
-            if superposition == "y":
-                super_posx = np.random.randint(0, 10)
-                super_posy = np.random.randint(0, 10)
-                super_pos_orientation = np.random.choice(["vertical", "horizontal"])
-                ship = Ships(possible_ships[ship_name], y, x, orientation, True, super_pos_orientation, super_posy, super_posx)
-                all_ships.append(ship)
-                if board.check_valid_position(ship):
-                    board.add_ship(ship)
-                else:
-                    continue
-            else:
-                ship = Ships(possible_ships[ship_name], y, x, orientation)
-                all_ships.append(ship)
-                if board.check_valid_position(ship):
-                    board.add_ship(ship)
-                else:
-                    continue
-            i +=1
-                
-
-def pass_computer():
-
-    """
-    Very repeating code so this function is for legibility.
-    """
-    os.system('cls' if os.name == 'nt' else "printf '\033c'")
-    input("Pass the computer to the other player. Press enter to continue.")
-    os.system('cls' if os.name == 'nt' else "printf '\033c'")
-
-def situation(player):
-    if player==1:
-        print("Your situation is:")
-        print("Your board:")
-        print("")
-        board1.print_board(True)
-        print("Your opponent's board:")
-        print("")
-        board2.print_board(False)
-    else:
-        print("Your situation is:")
-        print("Your board:")
-        print("")
-        board2.print_board(True)
-        print("Your opponent's board:")
-        print("")
-        board1.print_board(False)
-    input("Press enter to continue")
- 
 
 def single_player_game(board1, board2):
     # Remove everything from screen
@@ -264,7 +83,7 @@ def two_players_game(board_1, board_2):
     print("The boards are ready. Let's play!")
 
     while True:
-        situation(1)
+        situation(1, board1, board2)
         # Player 1 turn
         print("Player 1 turn\n")
         print("Choose the position to shoot.")
@@ -285,11 +104,11 @@ def two_players_game(board_1, board_2):
             break
 
         # Print the boards to the players
-        situation(1)
+        situation(1, board1, board2)
         pass_computer()
 
         # Player 2 turn
-        situation(2)
+        situation(2, board1, board2)
         print("Player 2 turn\n")
         print("Choose the position to shoot.")
         while True:
@@ -314,7 +133,7 @@ def two_players_game(board_1, board_2):
             break
 
         # Print the boards to the players
-        situation(2)
+        situation(2, board1, board2)
         pass_computer()
 
 
